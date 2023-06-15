040b614b00e37cfcfa8e2335ffcb243be078eddd
Oliver Lu <pa596bu500@gmail.com>
2023-06-15 04:15:50 +0000

    ini commit

---
+++ test/xt_ui_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+
+// import '../lib/wdgt/xtInfoBox.dart';
+// import '../lib/xt_helpers.dart';
+
+void main() {
+  // test('adds one to input values', () {
+  //   final calculator = Calculator();
+  //   expect(calculator.addOne(2), 3);
+  //   expect(calculator.addOne(-7), -6);
+  //   expect(calculator.addOne(0), 1);
+  // });
+  testWidgets('MyWidget has a title and message', (tester) async {
+    // Test code goes here.
+  });
+}
